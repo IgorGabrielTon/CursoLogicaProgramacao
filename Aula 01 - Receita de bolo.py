6bac69ce8ae292d31a1335541c2cab9a1206ff86
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-05-17 19:19:33 -0300

    Add files via upload

---
+++ Aula 01 - Receita de bolo.py
@@ -0,0 +1,55 @@
+# Algoritimo que ensina a fazer um bolo de chocolate
+# Feito por igor no dia 17/05/2022 ás 19:03
+
+print("========================================")
+print("")
+print("#----- Receita de bolo de chocolate-----#")
+print("")
+print("========================================")
+
+
+print("Ingredientes: ")
+
+print(""" 
+->Bolo
+
+2 ovos
+
+1 xícara (chá) de açúcar
+
+1 xícara (chá) de achocolatado ou chocolate em pó
+
+2 xícaras (chá) de farinha de trigo
+
+1 xícara (chá) de água (240 ml) fervida com 1 xícara (chá) de óleo
+
+1 colher (sopa) cheia de fermento em pó
+
+->Cobertura de brigadeiro
+
+1 caixa ou lata de leite condensado
+
+3 colheres (sopa) de chocolate em pó ou achocolatado
+
+1 colher (sopa) bem cheia de margarina
+
+Chocolate granulado para decorar    
+    
+""")
+
+
+print("""
+
+    Bolo
+Misture os ovos com açúcar e o chocolate em pó ou achocolatado.
+Adicione a farinha e a água com óleo e mexa bem. Incorpore o fermento.
+Despeje em fôrma retangular (30 x 24 cm) ou de sua preferência untada e enfarinhada.
+Leve ao forno alto preaquecido (250 ºC) por 20 minutos.
+Abaixe a temperatura para 180 ºC e continue assando até firmar (faça o teste do garfo ou palito).
+Corte em pedaços ainda na fôrma quente e despeje a cobertura.
+Espalhe granulado e sirva morno ou em temperatura ambiente.
+Cobertura de brigadeiro
+Misture o leite condensado com o chocolate em uma panela.
+Junte a margarina e leve ao fogo médio, mexendo sempre, até começar a desgrudar do fundo da panela (uns 8 minutos). Utilize quente.
+
+""")
