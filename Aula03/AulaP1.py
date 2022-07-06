2f19354491ff75d68af2deba85545fedf00acb57
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-07-06 10:46:22 -0300

    Create AulaP1.py

---
+++ Aula03/AulaP1.py
@@ -0,0 +1,21 @@
+#Operadores matemático (Aritmética)
+
+# Soma          +
+# Subtração     -
+# Multiplicação *
+# Divisão       /
+# Resto Divisão %
+# Porcentagem   (Digitar em formato decimal!)
+
+print(50*'=')
+print('Calculadora')
+print(50*'=')
+print('')
+
+numero1 = float( input('Digite um número: ') )
+numero2 = float( input('Digite um número: ') )
+
+print('A soma dos valores é: ', numero1+numero2 )
+print('A subtração dos valores é: ', numero1-numero2 )
+print('A Multiplicação dos valores é: ', numero1*numero2 )
+print('A Divisão dos valores é: ', numero1/numero2 )
