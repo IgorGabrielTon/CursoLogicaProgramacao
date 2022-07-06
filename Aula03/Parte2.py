d4c93f4fd9fc5d4d9d5787f9ffe0f3108b71111f
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-07-06 10:53:11 -0300

    Create Parte2.py

---
+++ Aula03/Parte2.py
@@ -0,0 +1,15 @@
+
+# Par ou Ímpar
+
+#Qual valor dividido por 2 que O RESTO É  0 é Par
+#Qual valor dividido por 2 que O RESTO É  1 é Ímpar
+
+
+# Multiplo 
+
+# 3 é Multiplo de 27 
+
+# Primeiro teste sempre o valor MAIOR % valor MENOR
+# Ver se é igual a ZERO (Multiplo), qual outro valor NÃO É Multiplo
+
+print(  54 % 5  )
