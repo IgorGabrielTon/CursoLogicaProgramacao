c06b1b4f02c70aed72a6103aac67e170368a5850
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-07-06 10:26:09 -0300

    Create OperadoresRelacionais.py

---
+++ Aula03/OperadoresRelacionais.py
@@ -0,0 +1,20 @@
+#Operações Númericas Relacionais
+
+# Maior que (>), 
+# Menor que (<), 
+# Igual (==), 
+# Maior ou Igual (>=), 
+# Menor ou Igual (<=), 
+# Diferente (!=)
+
+print('1º O valor 50 é maior que 30?')
+
+print('2º O valor 10 é menor que 90?')
+
+print('3º O valor 20 é igual a 20.001?')
+
+print('4º O valor 50 é maior ou igual a 50?')
+
+print('5º O valor 50 é menor ou igual 49?')
+
+print('6º O valor 50 é é diferente de 50?')
