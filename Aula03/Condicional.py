6951e6364965fb6bba7071b22c45ccbe56761cbf
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-07-06 10:32:31 -0300

    Create Condicional.py

---
+++ Aula03/Condicional.py
@@ -0,0 +1,15 @@
+# Se a comparação for True faça:
+    #Comando
+# Senão faça:
+    # Comando
+    
+Idade = int( input('Digite sua idade: ') )
+
+if Idade <= 18:
+    print('Você ainda não tem 18 anos!')
+    print('Não pode ter habilitação!')
+else:
+    print('Você já é de maior!')
+    print('Já pode dirigir!')
+
+print('Bye')
