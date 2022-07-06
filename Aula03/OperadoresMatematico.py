005df72087c75409f9273ba6529c2a04f0052420
Igor Gabriel Tonchichi <99445627+IgorGabrielTon@users.noreply.github.com>
2022-07-06 10:29:46 -0300

    Create OperadoresMatematico.py

---
+++ Aula03/OperadoresMatematico.py
@@ -0,0 +1,16 @@
+#Operadores matemático (Aritmética)
+
+# Soma          +
+# Subtração     -
+# Multiplicação *
+# Divisão       /
+# Resto Divisão %
+# Porcentagem   (Digitar em formato decimal!)
+
+print('Quanto é 50 + 90?')
+print('Quanto é 50 - 20 + 30?')
+print('Quanto é 2 * 10?')
+print('Quanto é 100 / 2?')
+print('Quanto é 10 % 2?')
+print('Quanto é 23% de 427?')
+print('Quanto é 16% de 50?')
